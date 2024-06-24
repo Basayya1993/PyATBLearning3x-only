6185a9471b9994680dfa2ea795ba3df1a040230d
Basayya Hiremath <167414629+Basayya1993@users.noreply.github.com>
2024-06-24 16:46:07 +0530

    Task_09.py

--- 24-June-2024/Task_09.py
+++ 24-June-2024/Task_09.py
@@ -17,7 +17,7 @@ count = 0
 for i in name:
     if i == "b" or i == "c" or i == "d" or i == "f" or i == "g" or \
             i == "h" or i == "j" or i == "k" or i == "l" or i == "m" or i == "n" or i == "o" or i == "p" or i == "q" or i == "r" or \
-            i == "s" or i == "t" or i == "u" or i == "v" or i == "w" or i == "x" or i == "y" or i == "z":
+            i == "s" or i == "t" or i == "v" or i == "w" or i == "x" or i == "y" or i == "z":
         count = count + 1
 print("Number of Consonants Present in:", name)
 print("Consonants:", count)
